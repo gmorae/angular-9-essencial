48341ac37b265760599b3bc60b00449871e41088
gmorae <gmmartins06@gmail.com>
2020-04-23 02:41:33 -0300

    Mostrando msg error

--- frontend/src/app/services/products.service.ts
+++ frontend/src/app/services/products.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, EMPTY } from 'rxjs';
 import { ProductsModel } from '../models/products.model';
 import { environment } from 'src/environments/environment';
-import { map } from 'rxjs/operators';
+import { map, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -19,38 +19,64 @@ export class ProductsService {
   getAll(): Observable<Array<ProductsModel>> {
     return this._http.get(`${environment.api}/products`)
       .pipe(
-        map((res: any) => {          
+        map((res: any) => {
           return res
-        })
+        }),
+        catchError(e => this.errorHandle(e))
       )
   }
 
   getById(id: number): Observable<ProductsModel> {
     return this._http.get(`${environment.api}/products/${id}`)
       .pipe(
-        map((res: any) => {          
+        map((res: any) => {
           return res
-        })
+        }),
+        catchError(e => this.errorHandle(e))
       )
   }
 
   createProduct(product: ProductsModel) {
-    return this._http.post(`${environment.api}/products`, product);
+    return this._http.post(`${environment.api}/products`, product)
+      .pipe(
+        map((res: any) => {
+          return res
+        }),
+        catchError(e => this.errorHandle(e))
+      )
   }
 
   updateProduct(product: ProductsModel) {
-    return this._http.put(`${environment.api}/products/${product.id}`, product);
+    return this._http.put(`${environment.api}/products/${product.id}`, product)
+      .pipe(
+        map((res: any) => {
+          return res
+        }),
+        catchError(e => this.errorHandle(e))
+      )
   }
 
   deleteProduct(id: number) {
-    return this._http.delete(`${environment.api}/products/${id}`);
+    return this._http.delete(`${environment.api}/products/${id}`)
+      .pipe(
+        map((res: any) => {
+          return res
+        }),
+        catchError(e => this.errorHandle(e))
+      )
   }
 
-  showMessage(msg: string): void {
+  errorHandle(e: any): Observable<any> {
+    this.showMessage('Ocorreu um erro', true)
+    return EMPTY
+  }
+
+  showMessage(msg: string, error: boolean = false): void {
     this._snackBar.open(msg, 'X', {
       duration: 3000,
       horizontalPosition: 'right',
-      verticalPosition: 'top'
+      verticalPosition: 'top',
+      panelClass: error ? ['msg-error'] : ['msg-success']
     })
   }
 }
